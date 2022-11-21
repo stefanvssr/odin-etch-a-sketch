524b0e7998eb2513a1161cd2e36ec9c76aee3eab
Stefan Visser <me@stefanvssr.dev>
2022-11-21 22:06:14 +0100

    create grid with JS

---
+++ script.js
@@ -0,0 +1,19 @@
+const gridContainer = document.querySelector(".container");
+const createGridItem = document.createElement("div");
+let gridXY = 16;
+
+function calcGrid() {
+    return gridXY * gridXY;
+}
+
+function createGrid() {
+
+    let totalGridItems = calcGrid();
+
+    for (let i = 1; i <= totalGridItems; i++) {
+        gridContainer.appendChild(createGridItem.cloneNode(true));
+        console.log(i);
+    }
+}
+
+createGrid();
