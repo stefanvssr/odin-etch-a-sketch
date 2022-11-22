573e4774cd2542e64e618ff28555e508b0000b8b
Stefan Visser <me@stefanvssr.dev>
2022-11-22 22:17:23 +0100

    Add hover over effect on grid items

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const gridContainer = document.querySelector(".container");
-const createGridItem = document.createElement("div");
-createGridItem.classList.add("grid-item");
+const gridItem = document.createElement("div");
+gridItem.classList.add("grid-item");
 let gridXY = 16;
+
+// We need to put an event listener on the container of the items,
+// Otherwise we can't target dynamically items.
+// This is because of event delegation.
+gridContainer.addEventListener("mouseover", hoverItem);
 
 function calcGrid() {
     return gridXY * gridXY;
@@ -12,8 +17,16 @@ function createGrid() {
     let totalGridItems = calcGrid();
 
     for (let i = 1; i <= totalGridItems; i++) {
-        gridContainer.appendChild(createGridItem.cloneNode(true));
-        console.log(i);
+        gridContainer.appendChild(gridItem.cloneNode(true));
+    }
+}
+
+function hoverItem(e) {
+    
+    // We only want to target elements with the class of .grid-item
+    // This is because of event bubbeling
+    if(e.target.classList.contains('grid-item')) {
+        e.target.style.backgroundColor = "#000000";
     }
 }
 
